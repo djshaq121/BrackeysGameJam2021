ac5b5491a8673ccdf58ec509d4d057c83e0770ee
Sulaiman Maarij <32667906+Sulaiman96@users.noreply.github.com>
2021-02-15 02:11:41 +0000

    Implemented basic turret mesh and rotation

---
+++ Source/BrackeysGameJam2021/Pawns/PawnTurret.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PawnTurret.h"
+#include "Kismet/GameplayStatics.h"
+#include "GameFramework/Character.h"
+
+// Called when the game starts or when spawned
+void APawnTurret::BeginPlay()
+{
+	Super::BeginPlay();
+	GetWorld()->GetTimerManager().SetTimer(FireRateTimerHandle, this, &APawnTurret::CheckFireCondition, FireRate, true);
+	PlayerCharacter = UGameplayStatics::GetPlayerCharacter(this, 0);
+}
+
+// Called every frame
+void APawnTurret::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!PlayerCharacter || ReturnDistanceToPlayer() > FireRange) {
+		return;
+	}
+
+	RotateTurret(PlayerCharacter->GetActorLocation());
+}
+
+void APawnTurret::CheckFireCondition()
+{
+	if (!PlayerCharacter)
+		return;
+
+	if (ReturnDistanceToPlayer() <= FireRange) {
+		//Fire
+		Fire();
+	}
+}
+
+float APawnTurret::ReturnDistanceToPlayer()
+{
+	if (!PlayerCharacter)
+		return 0.0f;
+
+	return FVector::Dist(PlayerCharacter->GetActorLocation(), GetActorLocation());
+}
