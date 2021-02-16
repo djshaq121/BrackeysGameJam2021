22937fbbe17bd5dda65f25a9c531d40c54d2b1e8
Sulaiman Maarij <32667906+Sulaiman96@users.noreply.github.com>
2021-02-16 19:05:18 +0000

    Implemented health component

---
+++ Source/BrackeysGameJam2021/Components/HealthComponent.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HealthComponent.h"
+#include "BrackeysGameJam2021/Public/WaveGameMode.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values for this component's properties
+UHealthComponent::UHealthComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UHealthComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	Health = DefaultHealth;
+	AActor* MyOwner = GetOwner();
+	if (MyOwner) {
+		GetOwner()->OnTakeAnyDamage.AddDynamic(this, &UHealthComponent::TakeDamage);
+	}
+}
+
+void UHealthComponent::TakeDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser)
+{
+	if(Damage <= 0.0f)
+		return;
+
+	Health = FMath::Clamp(Health - Damage, 0.0f, DefaultHealth);
+
+	OnHealthChanged.Broadcast(this, Health, Damage, DamageType, InstigatedBy, DamageCauser);
+}
+
+
---
+++ Source/BrackeysGameJam2021/Components/HealthComponent.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+class AWaveGameMode;
+//class FOnHealthChangedSignature;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_SixParams(FOnHealthChangedSignature, UHealthComponent*, HealthComp, float, Health, float, Damage, const UDamageType*, DamageType, AController*, InstigatedBy, AActor*, DamageCauser);
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BRACKEYSGAMEJAM2021_API UHealthComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UHealthComponent();
+	
+	UPROPERTY(BlueprintAssignable, Category = "Event")
+	FOnHealthChangedSignature OnHealthChanged;
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere)
+	float DefaultHealth = 100.0f;
+	float Health = 0.0f;
+
+	UFUNCTION()
+	void TakeDamage(AActor* DamagedActor, float Damage, const UDamageType* DamageType, AController* InstigatedBy, AActor* DamageCauser);
+		
+};
